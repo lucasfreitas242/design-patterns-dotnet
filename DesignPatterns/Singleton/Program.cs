eec472ccf6239c97733e01ccd291e93f782cdc05
Lucas Freitas <lucasfrittas@gmail.com>
2024-06-18 19:48:02 -0300

    Singleton

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Singleton - Start\n");
+
+            Console.WriteLine($"Valor inicial: {Singleton.Instance.value}");
+
+            Console.WriteLine("Tentando criar instancia s1.");
+
+            Singleton s1 = Singleton.Instance;
+
+            Console.WriteLine($"Valor s1: {s1.value}");
+
+            Console.WriteLine("Tentando criar instancia s2.");
+
+            Singleton s2 = Singleton.Instance;
+
+            Console.WriteLine($"Valor s2: {s2.value}");
+
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Existe somente uma instancia (s1 é igual a s2)");
+            }
+            else
+            {
+                Console.WriteLine("Existem instancias diferentes (s1 e s2)");
+            }
+
+            Console.WriteLine("\nSingleton - End");
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance = new Singleton();
+
+        public int value = 0;
+
+        private Singleton()
+        {
+            Random rnd = new Random();
+            value = rnd.Next(20);
+        }
+
+        public static Singleton Instance { get { return instance; } }
+    }
+}
